e462a39f772c05e4f5d72a6996f86cfcf8f6a570
Denis Nikolaev <niko.den.vl@gmail.com>
2024-06-25 19:27:09 +0400

    [close #10] feat: create program slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,4 +6,41 @@ $(() => {
     $(".video__container").css({ background: "none" });
     $("#video-player").css({ display: "block" });
   });
+
+
+  initProgramSlider();
 });
+
+function showPreviousSlide($slides) {
+  $slides.currentSlide =
+    ($slides.currentSlide - 1 + $slides.length) % $slides.length;
+  updateSlider($slides);
+}
+
+function showNextSlide($slides) {
+  $slides.currentSlide = ($slides.currentSlide + 1) % $slides.length;
+  updateSlider($slides);
+}
+
+function updateSlider($slides) {
+  $slides.each(function (index) {
+    if (index === $slides.currentSlide) {
+      $(this).css({display: "grid"});
+    } else  {
+      $(this).css({display: "none"});
+    }
+  })
+}
+
+function initProgramSlider() {
+  const $programSlider = $(".program__slider");
+  const $prevButton = $programSlider.find(".slider-btn_prev");
+  const $nextButton = $programSlider.find(".slider-btn_next");
+  const $slides = $programSlider.find(".program__tour");
+  $slides.currentSlide = 0;
+
+  $prevButton.on("click", () => showPreviousSlide($slides));
+  $nextButton.on("click", () => showNextSlide($slides));
+
+  updateSlider($slides) 
+}
